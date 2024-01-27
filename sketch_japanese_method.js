6c7f9b914fc28512a816c48c84054a48efafdf98
Jimmy Nguyen Tuan Dung <79316058+himynameiszim@users.noreply.github.com>
2024-01-27 18:59:11 +0900

    Update and rename sketch_japanese.js to sketch_japanese_method.js

--- sketch_japanese_method.js
+++ sketch_japanese_method.js
@@ -1,6 +1,6 @@
-let gap = 25; 
-let groupGap = 100; // Gap between groups of lines 
-let colors = { 
+var gap = 25; 
+var groupGap = 100; // Gap between groups of lines 
+var colors = { 
   tens: [0, 0, 255], // Blue for the tens digit 
   units: [255, 0, 0] // Red for the units digit 
 };
@@ -8,7 +8,7 @@ let colors = {
 function setup() { 
   createCanvas(1920, 1080); 
   background(0); 
-  drawLines(num1, num2);
+  // drawLines(77, 77);
   
   textSize(32);
   fill(255); // White color
@@ -30,59 +30,59 @@ function drawLines(num1, num2) {
   const endX = startX + lineLength;
   const endY = startY + lineLength;
 
-  let intersections = [];
-  let intersectionCounts = { tens: 0, units: 0, mixed: 0 };
+  var intersections = [];
+  var intersectionCounts = { tens: 0, units: 0, mixed: 0 };
 
   push();
   translate(width / 2, height / 2); 
   rotate(PI / 4); 
   translate(-width / 2, -height / 2);
 
-  let linesAArray = [];
+  var linesAArray = [];
   stroke(getColor('tens')); 
-  for (let i = 0; i < linesA[0]; i++) {
-    let x1 = startX + i * gap;
-    let y1 = startY;
-    let x2 = startX + i * gap;
-    let y2 = endY;
+  for (var i = 0; i < linesA[0]; i++) {
+    var x1 = startX + i * gap;
+    var y1 = startY;
+    var x2 = startX + i * gap;
+    var y2 = endY;
     line(x1, y1, x2, y2);
     linesAArray.push({x1, y1, x2, y2, color: 'tens'});
   } 
 
   stroke(getColor('units')); 
-  for (let i = 0; i < linesA[1]; i++) {
-    let x1 = startX + (i + linesA[0]) * gap + groupGap;
-    let y1 = startY;
-    let x2 = startX + (i + linesA[0]) * gap + groupGap;
-    let y2 = endY;
+  for (var i = 0; i < linesA[1]; i++) {
+    var x1 = startX + (i + linesA[0]) * gap + groupGap;
+    var y1 = startY;
+    var x2 = startX + (i + linesA[0]) * gap + groupGap;
+    var y2 = endY;
     line(x1, y1, x2, y2);
     linesAArray.push({x1, y1, x2, y2, color: 'units'});
   }
 
-  let linesBArray = [];
+  var linesBArray = [];
   stroke(getColor('tens')); 
-  for (let i = 0; i < linesB[0]; i++) {
-    let x1 = startX;
-    let y1 = startY + i * gap;
-    let x2 = endX;
-    let y2 = startY + i * gap;
+  for (var i = 0; i < linesB[0]; i++) {
+    var x1 = startX;
+    var y1 = startY + i * gap;
+    var x2 = endX;
+    var y2 = startY + i * gap;
     line(x1, y1, x2, y2);
     linesBArray.push({x1, y1, x2, y2, color: 'tens'});
   } 
 
   stroke(getColor('units')); 
-  for (let i = 0; i < linesB[1]; i++) {
-    let x1 = startX;
-    let y1 = startY + (i + linesB[0]) * gap + groupGap;
-    let x2 = endX;
-    let y2 = startY + (i + linesB[0]) * gap + groupGap;
+  for (var i = 0; i < linesB[1]; i++) {
+    var x1 = startX;
+    var y1 = startY + (i + linesB[0]) * gap + groupGap;
+    var x2 = endX;
+    var y2 = startY + (i + linesB[0]) * gap + groupGap;
     line(x1, y1, x2, y2);
     linesBArray.push({x1, y1, x2, y2, color: 'units'});
   }
 
-  for (let lineA of linesAArray) {
-    for (let lineB of linesBArray) {
-      let intersection = getIntersection(lineA, lineB);
+  for (var lineA of linesAArray) {
+    for (var lineB of linesBArray) {
+      var intersection = getIntersection(lineA, lineB);
       if (intersection) {
         intersections.push(intersection);
         if (lineA.color === lineB.color) {
@@ -96,33 +96,33 @@ function drawLines(num1, num2) {
 
   stroke(255); 
   strokeWeight(5); 
-  for (let i = 0; i < intersections.length; i++) {
+  for (var i = 0; i < intersections.length; i++) {
     const [x, y] = intersections[i];
     point(x, y);
   }
 
   pop();
 
-  let product = intersectionCounts.tens * 100 + intersectionCounts.units * 1 + intersectionCounts.mixed * 10;
+  var product = intersectionCounts.tens * 100 + intersectionCounts.units * 1 + intersectionCounts.mixed * 10;
   textSize(32);
   fill(255); // White color
-  let tensProduct = intersectionCounts.tens * 100;
-  let unitsProduct = intersectionCounts.units * 1;
-  let mixedProduct = intersectionCounts.mixed * 10;
+  var tensProduct = intersectionCounts.tens * 100;
+  var unitsProduct = intersectionCounts.units * 1;
+  var mixedProduct = intersectionCounts.mixed * 10;
 
-  let detailedCalculation = `${intersectionCounts.tens} * 100 + ${intersectionCounts.mixed} * 10 + ${intersectionCounts.units} * 1 = ${product}`;
+  var detailedCalculation = `${intersectionCounts.tens} * 100 + ${intersectionCounts.mixed} * 10 + ${intersectionCounts.units} * 1 = ${product}`;
   text(num1 + " * " + num2 + " = " + detailedCalculation, 10, 50); // Display the product at position (10, 50)
   // text(detailedCalculation, 10, 80); // Display the detailed calculation at position (10, 80)
 }
 
 function getIntersection(lineA, lineB) {
-  let denominator = (lineA.x1 - lineA.x2) * (lineB.y1 - lineB.y2) - (lineA.y1 - lineA.y2) * (lineB.x1 - lineB.x2);
+  var denominator = (lineA.x1 - lineA.x2) * (lineB.y1 - lineB.y2) - (lineA.y1 - lineA.y2) * (lineB.x1 - lineB.x2);
   if (denominator === 0) {
     return null; // The lines are parallel
   }
 
-  let x = ((lineA.x1 * lineA.y2 - lineA.y1 * lineA.x2) * (lineB.x1 - lineB.x2) - (lineA.x1 - lineA.x2) * (lineB.x1 * lineB.y2 - lineB.y1 * lineB.x2)) / denominator;
-  let y = ((lineA.x1 * lineA.y2 - lineA.y1 * lineA.x2) * (lineB.y1 - lineB.y2) - (lineA.y1 - lineA.y2) * (lineB.x1 * lineB.y2 - lineB.y1 * lineB.x2)) / denominator;
+  var x = ((lineA.x1 * lineA.y2 - lineA.y1 * lineA.x2) * (lineB.x1 - lineB.x2) - (lineA.x1 - lineA.x2) * (lineB.x1 * lineB.y2 - lineB.y1 * lineB.x2)) / denominator;
+  var y = ((lineA.x1 * lineA.y2 - lineA.y1 * lineA.x2) * (lineB.y1 - lineB.y2) - (lineA.y1 - lineA.y2) * (lineB.x1 * lineB.y2 - lineB.y1 * lineB.x2)) / denominator;
 
   return [x, y];
 }
